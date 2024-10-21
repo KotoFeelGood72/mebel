75891463ab17fb0283b5431a0fa8c2bd7b6b6e0c
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-10-21 14:03:27 +0300

    ss

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_BASE_URL: string; // Или другие переменные окружения, которые вы используете
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_AUTH_BASE_URL: string;
+  // Добавьте другие переменные окружения, если они есть
 }
 
 interface ImportMeta {
--- vite.config.ts
+++ vite.config.ts
@@ -1,66 +1,69 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dotenv from "dotenv";
+import path from "path";
 
-// Загрузка переменных окружения в зависимости от окружения (development/production)
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+export default defineConfig(({ mode }) => {
+  // Загружаем файл .env для соответствующего режима
+  const env = loadEnv(mode, process.cwd());
 
-const apiUrl = process.env.VITE_API_BASE_URL;
-const authUrl = process.env.VITE_AUTH_BASE_URL;
+  // Теперь мы можем использовать переменные окружения
+  const apiUrl = env.VITE_API_BASE_URL;
+  const authUrl = env.VITE_AUTH_BASE_URL;
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: () => "main.js",
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name].[ext]",
+  return {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: () => "main.js",
+          entryFileNames: "assets/[name].js",
+          chunkFileNames: "assets/[name].js",
+          assetFileNames: "assets/[name].[ext]",
+        },
+      },
+      cssCodeSplit: false,
+    },
+
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-    cssCodeSplit: false,
-  },
-
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    define: {
+      __API_URL__: JSON.stringify(apiUrl),
+      __AUTH_URL__: JSON.stringify(authUrl),
     },
-  },
-  define: {
-    __API_URL__: JSON.stringify(apiUrl),
-    __SOCKET_URL__: JSON.stringify(authUrl),
-  },
-  server: {
-    port: 5173,
-    host: true,
-    strictPort: true,
-    watch: {
-      usePolling: true,
-    },
-    proxy: {
-      "/api": {
-        target: apiUrl, // Использование переменной окружения
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+    server: {
+      port: 5173,
+      host: true,
+      strictPort: true,
+      watch: {
+        usePolling: true,
       },
-      "/auth": {
-        target: authUrl, // Использование переменной окружения
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auth/, ""),
+      proxy: {
+        "/api": {
+          target: apiUrl, // Использование переменной окружения
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+        "/auth": {
+          target: authUrl, // Использование переменной окружения
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/auth/, ""),
+        },
       },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-        @import "@/assets/scss/_mixins.scss";
-        @import "@/assets/scss/_variables.scss";
-        `,
-        api: "modern-compiler",
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @import "@/assets/scss/_mixins.scss";
+          @import "@/assets/scss/_variables.scss";
+          `,
+        },
       },
     },
-  },
+  };
 });
